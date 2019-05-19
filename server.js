cc13c6680504e491f959bc55b11a4a252fda6188
enzofass <zoefassioli@gmail.com>
2019-05-19 12:10:20 -0400

    set html and html api routes

--- server.js
+++ server.js
@@ -1,49 +1,18 @@
 // Dependencies
 // =============================================================
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = 8080;
+const app = express();
+const PORT = 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-// Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "/app/public/home.html"));
-});
-
-// Basic route that sends the user first to the AJAX Page
-app.get("/survey", function(req, res) {
-  res.sendFile(path.join(__dirname, "/app/public/survey.html"));
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+require("./app/routing/htmlRoutes.js")(app);
 
 // Starts the server to begin listening
 // =============================================================
