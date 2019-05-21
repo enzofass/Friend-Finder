c0f473b20a30b7dbef058fb27aaf466682606fad
enzofass <zoefassioli@gmail.com>
2019-05-21 19:43:32 -0400

    fixed alert and comments

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,24 +6,28 @@ module.exports = function(app) {
     res.json(friendsData);
   });
 
-
-  // post
+  // post to API
   app.post("/api/friends", function(req, res) {
-
+    // current user object
     const userInput = req.body;
+    // array to store differences
     const arrayDifferences = [];
-
+    // outter loop to loop through all friends in API
     for (let i = 0; i < friendsData.length; i++) {
       let totalDifference = 0;
+      // inner loop to compare scores and find difference
       for (let j = 0; j < userInput.scores.length; j++) {
         totalDifference += Math.abs(
           parseInt(friendsData[i].scores[j]) - parseInt(userInput.scores[j])
         );
       }
+      // push total difference into array for each friend
       arrayDifferences.push(totalDifference);
 
       console.log(arrayDifferences);
     }
+    // best match is determined by lowest difference during compare
+    // grab index of lowest value
     const bestMatchIndex = arrayDifferences.indexOf(
       Math.min(...arrayDifferences)
     );
@@ -31,9 +35,9 @@ module.exports = function(app) {
       friendsData[bestMatchIndex].name,
       friendsData[bestMatchIndex].photo
     );
+    // return best match json
     res.json(friendsData[bestMatchIndex]);
-
+    // push current user to API
     friendsData.push(req.body);
-
   });
 };
