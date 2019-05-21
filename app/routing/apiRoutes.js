a167f64f04f71fc0c63e18e8c420319b1a68e289
enzofass <zoefassioli@gmail.com>
2019-05-20 23:46:00 -0400

    finished friend match and append to modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,12 +7,29 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", function(req, res) {
+    const userInput = req.body;
+    const arrayDifferences = [];
+
+    for (let i = 0; i < friendsData.length; i++) {
+      let totalDifference = 0;
+      for (let j = 0; j < userInput.scores.length; j++) {
+        totalDifference += Math.abs(
+          parseInt(friendsData[i].scores[j]) - parseInt(userInput.scores[j])
+        );
+      }
+      arrayDifferences.push(totalDifference);
+
+      console.log(arrayDifferences);
+    }
+    const bestMatchIndex = arrayDifferences.indexOf(
+      Math.min(...arrayDifferences)
+    );
+    console.log(
+      friendsData[bestMatchIndex].name,
+      friendsData[bestMatchIndex].photo
+    );
+    res.json(friendsData[bestMatchIndex]);
+
     friendsData.push(req.body);
-
-    const userInput = res.body;
-    console.log(userInput);
-    res.json({ ok: true });
-
-    // friend-finder logic goes here
   });
 };
